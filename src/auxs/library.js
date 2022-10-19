328c3955ddf6a7d1b6f9143bac97ed22a2932ea1
Dario Bnz <developfsa@gmail.com>
2022-10-18 22:57:16 -0300

    agregados ejercicios

--- src/auxs/library.js
+++ src/auxs/library.js
@@ -26,5 +26,11 @@ export const diccionario = {
 
 }
 
+// Trabajo Practico A: agregar propiedades al diccionario que consista en hacer fetch a 
+// SU API REST EJ. traer los datos de un usuario.
+
+//Trabajo Practico B: agregar una propiedad al diccionario que permita enviarle un array 
+//no mayor a 10 elementos, y que retorne el promedio de valores del array.
+
 
 
